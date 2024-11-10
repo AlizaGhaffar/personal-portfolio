00b26aba8f992f6eb2e13e27b83f6d1044b243fa
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-11-10 12:29:27 +0500

    Add files via upload

---
+++ src/static/Summary.tsx
@@ -0,0 +1,12 @@
+
+function Summary(){
+    return (<>
+        <p className="text-4xl mb-8">About me</p>
+        <article className="desktop:text-xl mobile:text-md" itemScope itemType="https://schema.org/author">
+            <p className="mb-5">I am AI undergraduate at SMIU'28 university pursuing cloud applied Generative AI engineering course at GIAIC
+                My main focus these days is to learn nextjs and react to build high performance web applications for desktop and mobile devices using the latest technologies. </p>
+        </article>
+    </>);
+}
+
+export default Summary;
